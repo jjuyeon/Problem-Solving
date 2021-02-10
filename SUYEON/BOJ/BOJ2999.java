81e44dd8e96350974834aa9e1874d2551df3916f
Suyeon Jin <suyeon.jin06@gmail.com>
2021-02-10 10:27:41 +0900

    Add another idea to calculate R : BOJ2999 비밀이메일

--- SUYEON/BOJ/BOJ2999.java
+++ SUYEON/BOJ/BOJ2999.java
@@ -7,8 +7,22 @@ public class BOJ2999 {
         Scanner sc = new Scanner(System.in);
         char[] arr = sc.next().toCharArray();
         int len = arr.length;
-        int R = findR(len);
-        int C = len/R;
+
+        // 구해보니 방법1 이나 방법2나 시간은 비슷했음;;
+
+        // 방법 1 (앞에서부터 탐색)
+//        int R = findR(len);
+//        int C = len/R;
+
+        // 방법2 (뒤에서부터 탐색 - 왜? R이 최대인 값을 구해야하므로)
+        int R = 1, C = len;
+        for(int i=(int)Math.sqrt(len); i>0; i--){ // R은 C보다 같거나 작으므로 Math.sqrt(제곱근)값을 넘지 못함
+            if(len%i == 0){
+                R = i;
+                C = len/i;
+                break;
+            }
+        }
 
         int arrIdx = 0;
         char[][] matrix = new char[R][C];
@@ -25,6 +39,7 @@ public class BOJ2999 {
         }
     }
 
+    // 방법1
     static int findR(int n){
         int result = 0;
         for(int i=1; i<n; i++){

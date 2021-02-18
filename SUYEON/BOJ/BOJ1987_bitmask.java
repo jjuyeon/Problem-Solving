76b3eff98f18a34af49292b9d3c3e93580c01685
Suyeon Jin <suyeon.jin06@gmail.com>
2021-02-19 02:50:24 +0900

    Fix: Remove boolean array (BOJ알파벳)
    방문배열 boolean배열을 없애도 됨
    왜? 비트마스킹으로 방문한 알파벳을 체크하고 있으므로(해당 위치도 같이 저장됨)

--- SUYEON/BOJ/BOJ1987_bitmask.java
+++ SUYEON/BOJ/BOJ1987_bitmask.java
@@ -5,7 +5,6 @@ import java.util.*;
 
 public class BOJ1987_bitmask {
     static int R, C, ans;
-    static boolean[][] v;
     static char[][] board;
 
     public static void main(String[] args) throws IOException {
@@ -19,7 +18,6 @@ public class BOJ1987_bitmask {
             board[i] = br.readLine().toCharArray();
         }
 
-        v = new boolean[R][C];
         ans = 0;
         dfs(0,0,1, 1<<(board[0][0]-'A'));
         System.out.print(ans);
@@ -28,21 +26,23 @@ public class BOJ1987_bitmask {
     static int[] dr = {-1,1,0,0};
     static int[] dc = {0,0,-1,1};
     private static void dfs(int r, int c, int cnt, int visitedAlpha){
+        ans = Math.max(ans, cnt);
+
+        if(cnt == 26){ // 모든 알파벳을 다 모았을 때(최대값) 끝낸다
+            return;
+        }
+
         for(int i=0; i<4; i++){
             int nr = r + dr[i];
             int nc = c + dc[i];
 
-            if(nr<0 || nr>=R || nc<0 || nc>=C || v[nr][nc]){ // 범위 벗어나고, 방문한 위치면 패스
+            if(nr<0 || nr>=R || nc<0 || nc>=C){ // 범위 벗어남
                 continue;
             }
             if((visitedAlpha & (1<<(board[nr][nc]-'A'))) == 0){ // 아직 만난적 없는 알파벳일 때
-                v[nr][nc] = true;
                 dfs(nr, nc, cnt+1, visitedAlpha|(1<<(board[nr][nc]-'A')));
-                v[nr][nc] = false;
             }
         }
-
-        ans = Math.max(ans, cnt);
     }
 
 }

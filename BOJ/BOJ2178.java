6f35c39b66269679233986212c607820d57be7c6
Suyeon Jin <suyeon.jin06@gmail.com>
2021-07-21 00:10:01 +0900

    BFS: BOJ2178 미로탐색_input 방식 수정

--- BOJ/BOJ2178.java
+++ BOJ/BOJ2178.java
@@ -1,8 +1,12 @@
 package BOJ;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class BOJ2178 {
 
@@ -20,14 +24,16 @@ public class BOJ2178 {
         }
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        m = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
         arr = new int[n+1][m+1];
         for (int i = 1; i <= n; i++) {
+            String str = br.readLine();
             for (int j = 1; j <= m; j++) {
-                arr[i][j] = sc.nextInt();
+                arr[i][j] = str.charAt(j-1) - '0';
             }
         }
 
@@ -41,17 +47,19 @@ public class BOJ2178 {
         Queue<Point> queue = new LinkedList<>();
         queue.offer(new Point(1, 1, 1));
 
-        boolean[][] visited = new boolean[n][m];
+        boolean[][] visited = new boolean[n+1][m+1];
         while(!queue.isEmpty()) {
             Point now = queue.poll();
+
             if(now.x == n && now.y == m) {
                 System.out.println(now.cnt);
                 return;
             }
+
             for (int i = 0; i < 4; i++) {
                 int nx = now.x + dx[i];
                 int ny = now.y + dy[i];
-                if(nx < 1 || nx > n || ny < 1 || ny > m || visited[nx][ny]) {
+                if(nx < 1 || nx > n || ny < 1 || ny > m || visited[nx][ny] || arr[nx][ny] == 0) {
                     continue;
                 }
                 visited[nx][ny] = true;
